bd5ec2505a5579ff0e5e7152b8ce0aeb9da4501f
3kishore <gokulkishore393@gmail.com>
2025-01-07 08:59:57 +0530

    Feature: home page folder structure pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CameraComponent } from './camera/camera.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CameraComponent],
+  imports: [RouterOutlet, CameraComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
